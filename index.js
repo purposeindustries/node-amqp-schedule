91a90efd88e266a9fc2591581e19474a0df296c0
Dányi Bence <madbence@gmail.com>
2013-12-04 21:29:12 +0100

    Basic implementation added

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function scheduler(conn, opt) {
+  opt = opt || {};
+  opt.prefix = opt.prefix || 'schedule.';
+  opt.threshold = opt.threshold || 10000;
+  return function publish(exchange, route, message, delay, options, fn) {
+    if(delay instanceof Date) {
+      delay = delay.getTime() - Date.now();
+    }
+    var name = opt.prefix + delay;
+    var queue = conn.queue(name, {
+      durable: true,
+      autoDelete: true,
+      arguments: {
+        'x-dead-letter-exchange': exchange,
+        'x-dead-letter-routing-key': route,
+        'x-message-ttl': delay,
+        'x-expires': delay + opt.threshold
+      }
+    }, function() {
+      conn.publish(name, message, options, fn);
+    });
+  };
+};
